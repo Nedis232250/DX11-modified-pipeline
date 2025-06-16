67bbea3875f589d24129127a2039400dad60615e
Nedis232250 <97570089+Nedis232250@users.noreply.github.com>
2025-06-15 20:19:53 -0400

    Update rasterizer.hlsl

--- rasterizer.hlsl
+++ rasterizer.hlsl
@@ -1,82 +1,82 @@
-RWBuffer<unsigned int> screen_buf : register(u0);
+RWBuffer<uint> screen_buf : register(u0);
 RWBuffer<float> z_buf : register(u1);
-RWBuffer<float> positions : register(u2);
-RWBuffer<float> colors : register(u3);
-RWBuffer<unsigned int> misc : register(u4);
-RWBuffer<unsigned int> texture_ : register(u5);
+StructuredBuffer<float> p : register(t0);
+StructuredBuffer<float> c : register(t1);
+StructuredBuffer<uint> misc : register(t2);
+StructuredBuffer<float> tri_meta : register(t3);
 
 [numthreads(1024, 1, 1)]
 void main(uint3 threadID : SV_DispatchThreadID, uint3 groupID : SV_GroupID) {
-    unsigned int index = threadID.x + groupID.x * 1024;
-
-    if (index < misc[2]) {
-        unsigned int x1 = positions[0 + index * 9];
-        unsigned int y1 = positions[1 + index * 9];
-        unsigned int z1 = positions[2 + index * 9];
-        unsigned int x2 = positions[3 + index * 9];
-        unsigned int y2 = positions[4 + index * 9];
-        unsigned int z2 = positions[5 + index * 9];
-        unsigned int x3 = positions[6 + index * 9];
-        unsigned int y3 = positions[7 + index * 9];
-        unsigned int z3 = positions[8 + index * 9];
-        unsigned int width = misc[0];
-        unsigned int height = misc[1];
-        float4 color1 = { colors[0 + index * 13], colors[1 + index * 13], colors[2 + index * 13], colors[3 + index * 13] };
-        float4 color2 = { colors[4 + index * 13], colors[5 + index * 13], colors[6 + index * 13], colors[7 + index * 13] };
-        float4 color3 = { colors[8 + index * 13], colors[9 + index * 13], colors[10 + index * 13], colors[11 + index * 13] };
+    uint i = threadID.x;
+    uint local = i % 144;
+    uint t = i * rcp(144.0f);
+    uint qx = local % 12;
+    uint qy = local * rcp(12.0f);
+    
+    if (i < (misc[2] * 144)) {
+        float2 v1 = float2(p[t * 9], p[t * 9 + 1]);
+        float2 v2 = float2(p[t * 9 + 3], p[t * 9 + 4]);
+        float2 v3 = float2(p[t * 9 + 6], p[t * 9 + 7]);
+        float3 zs = float3(p[t * 9 + 2], p[t * 9 + 5], p[t * 9 + 8]);
+        float3 c1 = float3(c[t * 13], c[t * 13 + 1], c[t * 13 + 2]);
+        float3 c2 = float3(c[t * 13 + 4], c[t * 13 + 5], c[t * 13 + 6]);
+        float3 c3 = float3(c[t * 13 + 8], c[t * 13 + 9], c[t * 13 + 10]);
+        float3 alphas = float3(c[t * 13 + 3], c[t * 13 + 7], c[t * 13 + 11]);
+        float l1, l2, l3, a;
+        uint hit = 0;
         
-        // 0.0 for textures, 1.0 for colors, and when textures color.z/color.b doesnt matter
-        float mode = colors[12 + index * 13];
-
-        x1 = clamp(x1, 0.0f, (float) (width - 1));
-        y1 = clamp(y1, 0.0f, (float) (height - 1));
-        x2 = clamp(x2, 0.0f, (float) (width - 1));
-        y2 = clamp(y2, 0.0f, (float) (height - 1));
-        x3 = clamp(x3, 0.0f, (float) (width - 1));
-        y3 = clamp(y3, 0.0f, (float) (height - 1));
-
-        float minx = (x1 < x2 ? (x1 < x3 ? x1 : x3) : (x2 < x3 ? x2 : x3));
-        float maxx = (x1 > x2 ? (x1 > x3 ? x1 : x3) : (x2 > x3 ? x2 : x3));
-        float miny = (y1 < y2 ? (y1 < y3 ? y1 : y3) : (y2 < y3 ? y2 : y3));
-        float maxy = (y1 > y2 ? (y1 > y3 ? y1 : y3) : (y2 > y3 ? y2 : y3));
+        uint width = uint(tri_meta[t * 5 + 2]) - uint(tri_meta[t * 5]);
+        uint height = uint(tri_meta[t * 5 + 3]) - uint(tri_meta[t * 5 + 1]);
         
-        for (unsigned int x = minx; x < maxx; x++) {
-            for (unsigned int y = miny; y < maxy; y++) {
-                float denom = (y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3);
-                if (denom == 0)
-                    continue;
-
-                float l1 = ((y2 - y3) * (x - x3) + (x3 - x2) * (y - y3)) / denom;
-                float l2 = ((y3 - y1) * (x - x3) + (x1 - x3) * (y - y3)) / denom;
-                float l3 = 1.0 - l1 - l2;
+        uint minx = uint(tri_meta[t * 5]) + floor((float(qx) * rcp(12.0f)) * float(width));
+        uint miny = uint(tri_meta[t * 5 + 1]) + floor((float(qy) * rcp(12.0f)) * float(height));
+        uint maxx = minx + ceil(float(width) * rcp(12.0f));
+        uint maxy = miny + ceil(float(height) * rcp(12.0f));
+        
+        l1 = ((float(minx) - v2.x) * (v3.y - v2.y) - (float(miny) - v2.y) * (v3.x - v2.x)) * tri_meta[t * 5 + 4];
+        l2 = ((float(minx) - v3.x) * (v1.y - v3.y) - (float(miny) - v3.y) * (v1.x - v3.x)) * tri_meta[t * 5 + 4];
+        l3 = 1.0f - l2 - l1;
+        
+        if (l1 >= 0.0f && l2 >= 0.0f && l3 >= 0.0f)
+            hit++;
+        
+        l1 = ((float(maxx) - v2.x) * (v3.y - v2.y) - (float(miny) - v2.y) * (v3.x - v2.x)) * tri_meta[t * 5 + 4];
+        l2 = ((float(maxx) - v3.x) * (v1.y - v3.y) - (float(miny) - v3.y) * (v1.x - v3.x)) * tri_meta[t * 5 + 4];
+        l3 = 1.0f - l2 - l1;
+        
+        if (l1 >= 0.0f && l2 >= 0.0f && l3 >= 0.0f)
+            hit++;
+        
+        l1 = ((float(minx) - v2.x) * (v3.y - v2.y) - (float(maxy) - v2.y) * (v3.x - v2.x)) * tri_meta[t * 5 + 4];
+        l2 = ((float(minx) - v3.x) * (v1.y - v3.y) - (float(maxy) - v3.y) * (v1.x - v3.x)) * tri_meta[t * 5 + 4];
+        l3 = 1.0f - l2 - l1;
+        
+        if (l1 >= 0.0f && l2 >= 0.0f && l3 >= 0.0f)
+            hit++;
+        
+        l1 = ((float(maxx) - v2.x) * (v3.y - v2.y) - (float(maxy) - v2.y) * (v3.x - v2.x)) * tri_meta[t * 5 + 4];
+        l2 = ((float(maxx) - v3.x) * (v1.y - v3.y) - (float(maxy) - v3.y) * (v1.x - v3.x)) * tri_meta[t * 5 + 4];
+        l3 = 1.0f - l2 - l1;
+        
+        if (l1 >= 0.0f && l2 >= 0.0f && l3 >= 0.0f)
+            hit++;
+        
+        if (hit == 0)
+            return;
+        
+        if (c[t * 13 + 12] == 1.0f) {
+            for (uint x = minx; x < maxx; x++) {
+                for (uint y = miny; y < maxy; y++) {
+                    l1 = ((float(x) - v2.x) * (v3.y - v2.y) - (float(y) - v2.y) * (v3.x - v2.x)) * tri_meta[t * 5 + 4];
+                    l2 = ((float(x) - v3.x) * (v1.y - v3.y) - (float(y) - v3.y) * (v1.x - v3.x)) * tri_meta[t * 5 + 4];
+                    l3 = 1.0f - l2 - l1;
                 
-                if (l1 >= 0 && l2 >= 0 && l3 >= 0) {
-                    float z_interpolated = clamp(l1 * z1 + l2 * z2 + l3 * z3, 0.0f, 255.0f);
-                    
-                    if ((z_buf[y * width + x] > z_interpolated) && mode == 1.0f) {
-                        float alpha = clamp(l1 * color1.a + l2 * color2.a + l3 * color3.a, 0.0f, 1.0f);
-                        float r = clamp(l1 * color1.r + l2 * color2.r + l3 * color3.r, 0.0f, 1.0f) * alpha + ((screen_buf[y * width + x] >> 16 & 0xFF) / 255.0f) * (1.0 - alpha);
-                        float g = clamp(l1 * color1.g + l2 * color2.g + l3 * color3.g, 0.0f, 1.0f) * alpha + ((screen_buf[y * width + x] >> 8 & 0xFF) / 255.0f) * (1.0 - alpha);
-                        float b = clamp(l1 * color1.b + l2 * color2.b + l3 * color3.b, 0.0f, 1.0f) * alpha + ((screen_buf[y * width + x] & 0xFF) / 255.0f) * (1.0 - alpha);
-
-                        screen_buf[(unsigned int) (y * width + x)] = (int) (r * 255) << 16 | (int) (g * 255) << 8 | (int) (b * 255);
-                        z_buf[y * width + x] = z_interpolated;
-                    } else if ((z_buf[y * width + x] > z_interpolated) && mode == 0.0f) {
-                        float u = l1 * color1.x + l2 * color2.x + l3 * color3.x;
-                        float v = l1 * color1.y + l2 * color2.y + l3 * color3.y;
-                        u = clamp(u, 0.0f, 1.0f);
-                        v = clamp(v, 0.0f, 1.0f);
-                        
-                        unsigned int tx = clamp((uint) (u * texture_[0]), 0u, texture_[0] - 1u);
-                        unsigned int ty = clamp((uint) (v * texture_[1]), 0u, texture_[1] - 1u);
-                        unsigned int tex_color = texture_[2 + ty * texture_[0] + tx];
-                        float alpha = clamp(l1 * color1.a + l2 * color2.a + l3 * color3.a, 0.0f, 1.0f);
-                        float r = ((tex_color >> 16) & 0xFF) * alpha / 255.0f + ((screen_buf[y * width + x] >> 16 & 0xFF) / 255.0f) * (1.0 - alpha);
-                        float g = ((tex_color >> 8) & 0xFF) * alpha / 255.0f + ((screen_buf[y * width + x] >> 8 & 0xFF) / 255.0f) * (1.0 - alpha);
-                        float b = ((tex_color) & 0xFF) * alpha / 255.0f + ((screen_buf[y * width + x] & 0xFF) / 255.0f) * (1.0 - alpha);
-                        
-                        screen_buf[(unsigned int) (y * width + x)] = (int) (r * 255) << 16 | (int) (g * 255) << 8 | (int) (b * 255);
-                        z_buf[y * width + x] = z_interpolated;
+                    if (l1 >= 0.0f && l2 >= 0.0f && l3 >= 0.0f && z_buf[1540 * y + x] > (l1 * zs.x + l2 * zs.y + l3 * zs.z)) {
+                        a = l1 * alphas.r + l2 * alphas.g + l3 * alphas.b;
+                        screen_buf[1540 * y + x] = uint(((clamp(l1 * c1.r + l2 * c2.r + l3 * c3.r, 0.0f, 1.0f) * a + (1 - a) * ((screen_buf[1540 * y + x] >> 16) & 0xFF) * rcp(255.0f))) * 255u) << 16 |
+                                                   uint(((clamp(l1 * c1.g + l2 * c2.g + l3 * c3.g, 0.0f, 1.0f) * a + (1 - a) * ((screen_buf[1540 * y + x] >> 8) & 0xFF) * rcp(255.0f))) * 255u) << 8 |
+                                                   uint(((clamp(l1 * c1.b + l2 * c2.b + l3 * c3.b, 0.0f, 1.0f) * a + (1 - a) * (screen_buf[1540 * y + x] & 0xFF) * rcp(255.0f))) * 255u);
+                        z_buf[1540 * y + x] = (l1 * zs.x + l2 * zs.y + l3 * zs.z);
                     }
                 }
             }
