6771691acbc30b6f35a935b4b35825bc03d0623c
Nedis232250 <97570089+Nedis232250@users.noreply.github.com>
2025-07-31 18:34:05 -0400

    Update rasterizer.hlsl

--- rasterizer.hlsl
+++ rasterizer.hlsl
@@ -1,1 +1,79 @@
+struct Fragment {
+    float4 color1;
+    float4 color2;
+    float depth1;
+    float depth2;
+};
 
+RWStructuredBuffer<uint> screen_buf : register(u0);
+RWStructuredBuffer<float> z_buf : register(u1);
+RWStructuredBuffer<Fragment> fragment_buffer : register(u2);
+StructuredBuffer<float> p : register(t0);
+StructuredBuffer<float> c : register(t1);
+StructuredBuffer<uint> misc : register(t2);
+StructuredBuffer<float> tri_meta : register(t3);
+
+[numthreads(256, 1, 1)]
+void main(uint3 threadID : SV_DispatchThreadID, uint3 groupID : SV_GroupID) {
+    uint i = threadID.x;
+    uint local = i % 144;
+    uint t = i * rcp(144.0f);
+    uint qx = local % 12;
+    uint qy = local * rcp(12.0f);
+    
+    if (i < (misc[2] * 144)) {
+        float2 v1 = float2(p[t * 9], p[t * 9 + 1]);
+        float2 v2 = float2(p[t * 9 + 3], p[t * 9 + 4]);
+        float2 v3 = float2(p[t * 9 + 6], p[t * 9 + 7]);
+        float3 zs = float3(p[t * 9 + 2], p[t * 9 + 5], p[t * 9 + 8]);
+        float3 c1 = float3(c[t * 13], c[t * 13 + 1], c[t * 13 + 2]);
+        float3 c2 = float3(c[t * 13 + 4], c[t * 13 + 5], c[t * 13 + 6]);
+        float3 c3 = float3(c[t * 13 + 8], c[t * 13 + 9], c[t * 13 + 10]);
+        float3 as = float3(c[t * 13 + 3], c[t * 13 + 7], c[t * 13 + 11]);
+        float l1, l2, l3;
+        uint mode = c[t * 13 + 12];
+        
+        uint width = uint(tri_meta[t * 5 + 2]) - uint(tri_meta[t * 5]);
+        uint height = uint(tri_meta[t * 5 + 3]) - uint(tri_meta[t * 5 + 1]);
+        
+        uint minx = uint(tri_meta[t * 5]) + floor((float(qx) * rcp(12.0f)) * float(width));
+        uint miny = uint(tri_meta[t * 5 + 1]) + floor((float(qy) * rcp(12.0f)) * float(height));
+        uint maxx = minx + ceil(float(width) * rcp(12.0f));
+        uint maxy = miny + ceil(float(height) * rcp(12.0f));
+        
+        if (c[t * 13 + 12] == 1.0f) {
+            for (uint x = minx; x < maxx; x++) {
+                for (uint y = miny; y < maxy; y++) {
+                    l1 = ((float(x) - v2.x) * (v3.y - v2.y) - (float(y) - v2.y) * (v3.x - v2.x)) * tri_meta[t * 5 + 4];
+                    l2 = ((float(x) - v3.x) * (v1.y - v3.y) - (float(y) - v3.y) * (v1.x - v3.x)) * tri_meta[t * 5 + 4];
+                    l3 = 1.0f - l2 - l1;
+                
+                    if (l1 >= 0.0f && l2 >= 0.0f && l3 >= 0.0f) {
+                        float3 rgb_src = l1 * c1 + l2 * c2 + l3 * c3;
+                        float r = saturate(rgb_src.r);
+                        float g = saturate(rgb_src.g);
+                        float b = saturate(rgb_src.b);
+
+                        screen_buf[1540 * y + x] = 0xFF00FF;
+                        
+                        if ((l1 * zs.x + l2 * zs.y + l3 * zs.z) < fragment_buffer[1540 * y + x].depth1) {
+                            Fragment f = {
+                                { r, g, b, l1 * as[0] + l2 * as[1] + l3 * as[2] }, fragment_buffer[1540 * y + x].color2,
+                                (l1 * zs.x + l2 * zs.y + l3 * zs.z), fragment_buffer[1540 * y + x].depth2
+                            };
+                        
+                            fragment_buffer[1540 * y + x] = f;
+                        } else if ((l1 * zs.x + l2 * zs.y + l3 * zs.z) < fragment_buffer[1540 * y + x].depth2) {
+                            Fragment f = {
+                                fragment_buffer[1540 * y + x].color1, { r, g, b, l1 * as[0] + l2 * as[1] + l3 * as[2] },
+                                fragment_buffer[1540 * y + x].depth1, (l1 * zs.x + l2 * zs.y + l3 * zs.z)
+                            };
+                        
+                            fragment_buffer[1540 * y + x] = f;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
