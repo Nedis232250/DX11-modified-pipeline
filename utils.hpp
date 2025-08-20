3f09b56a5c3bb0eb44a2de5a7d161819a0702c9d
Nedis232250 <97570089+Nedis232250@users.noreply.github.com>
2025-08-20 03:00:07 +0200

    Update utils.hpp

--- utils.hpp
+++ utils.hpp
@@ -27,10 +27,6 @@ void attach_console() {
 	if (AllocConsole()) {
 		FILE* file;
 		freopen_s(&file, "CONOUT$", "w", stdout);
-		std::cout << "Console attached successfully!" << std::endl;
-	}
-	else {
-		std::cerr << "Failed to attach a console!" << std::endl;
 	}
 }
 
@@ -85,90 +81,90 @@ std::vector<unsigned int> read_bmp(std::string file_name) {
 }
 
 class Shader {
-private:
-	HRESULT hr;
-	ID3D11Device* device;
-	D3D_FEATURE_LEVEL level;
-	ID3D11DeviceContext* ctx;
-	ID3DBlob* error_blob;
-	ID3DBlob* shader_blob;
-public:
-	Shader(void) {};
-	~Shader() {
-		if (shader_blob) shader_blob->Release();
-	}
-
-	void Shader_device(ID3D11Device* dev, ID3D11DeviceContext* ctx_, D3D_FEATURE_LEVEL features) {
-		device = dev;
-		ctx = ctx_;
-		level = features;
-	}
-
-	HRESULT create_D3D11_buffer(ID3D11Buffer** buf, const void* dat_in, unsigned int size, unsigned char item_size, unsigned int type, unsigned int flags, unsigned int CPU_flags, D3D11_USAGE usage) {
-		D3D11_BUFFER_DESC buffer_description = { };
-		buffer_description.ByteWidth = size;
-		buffer_description.StructureByteStride = item_size;
-		buffer_description.BindFlags = type;
-		buffer_description.CPUAccessFlags = CPU_flags;
-		buffer_description.MiscFlags = flags;
-		buffer_description.Usage = usage;
-
-		D3D11_SUBRESOURCE_DATA data_in = { };
-		if (dat_in) {
-			data_in.pSysMem = dat_in;
-		}
-
-		return device->CreateBuffer(&buffer_description, dat_in ? &data_in : nullptr, buf);
-	}
-
-	HRESULT compile_shader(LPCWSTR file_name, LPCSTR entrypoint) {
+	private:
+		HRESULT hr;
+		ID3D11Device* device;
+		D3D_FEATURE_LEVEL level;
+		ID3D11DeviceContext* ctx;
 		ID3DBlob* error_blob;
-		return D3DCompileFromFile(file_name, nullptr, nullptr, entrypoint, "cs_5_0", 0, 0, &shader_blob, &error_blob);
-	}
-
-	ID3DBlob* get_error_blob() {
-		return error_blob;
-	}
-
-	ID3DBlob* get_shader_blob() {
-		return shader_blob;
-	}
-
-	HRESULT create_UAV(ID3D11Resource* buf, ID3D11UnorderedAccessView** UAV, unsigned int size, DXGI_FORMAT format) {
-		D3D11_UNORDERED_ACCESS_VIEW_DESC UAV_desc = { };
-		UAV_desc.Format = format;
-		UAV_desc.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
-		UAV_desc.Buffer.FirstElement = 0;
-		UAV_desc.Buffer.NumElements = size;
-
-		return device->CreateUnorderedAccessView(buf, &UAV_desc, UAV);
-	}
-
-	HRESULT create_SRV(ID3D11Resource* buf, ID3D11ShaderResourceView** SRV, unsigned int size, D3D11_SRV_DIMENSION dimension) {
-		D3D11_SHADER_RESOURCE_VIEW_DESC SRV_desc = { };
-		SRV_desc.Format = DXGI_FORMAT_UNKNOWN;
-		SRV_desc.ViewDimension = dimension;
-		SRV_desc.Buffer.NumElements = size;
-		SRV_desc.Buffer.FirstElement = 0;
-
-		return device->CreateShaderResourceView(buf, &SRV_desc, SRV);
-	}
-
-	template<typename T> T* retreive_D3D11_process(ID3D11Resource* CPU_buf, ID3D11Resource* GPU_buf) {
-		D3D11_MAPPED_SUBRESOURCE map_r = { };
-		ctx->CopyResource(CPU_buf, GPU_buf);
-
-		HRESULT hr = ctx->Map(CPU_buf, 0, D3D11_MAP_READ, 0, &map_r);
-		
-		if (FAILED(hr)) {
-			std::cout << "Mapping failed. HRESULT: " << std::hex << hr << "\n";
-			return 0;
+		ID3DBlob* shader_blob;
+	public:
+		Shader(void) {};
+		~Shader() {
+			if (shader_blob) shader_blob->Release();
 		}
 
-		T* dat = (T*)map_r.pData;
-		ctx->Unmap(CPU_buf, 0);
+		void Shader_device(ID3D11Device* dev, ID3D11DeviceContext* ctx_, D3D_FEATURE_LEVEL features) {
+			device = dev;
+			ctx = ctx_;
+			level = features;
+		}
 
-		return dat;
-	}
+		HRESULT create_D3D11_buffer(ID3D11Buffer** buf, const void* dat_in, unsigned int size, unsigned char item_size, unsigned int type, unsigned int flags, unsigned int CPU_flags, D3D11_USAGE usage) {
+			D3D11_BUFFER_DESC buffer_description = { };
+			buffer_description.ByteWidth = size;
+			buffer_description.StructureByteStride = item_size;
+			buffer_description.BindFlags = type;
+			buffer_description.CPUAccessFlags = CPU_flags;
+			buffer_description.MiscFlags = flags;
+			buffer_description.Usage = usage;
+
+			D3D11_SUBRESOURCE_DATA data_in = { };
+			if (dat_in) {
+				data_in.pSysMem = dat_in;
+			}
+
+			return device->CreateBuffer(&buffer_description, dat_in ? &data_in : nullptr, buf);
+		}
+
+		HRESULT compile_shader(LPCWSTR file_name, LPCSTR entrypoint) {
+			ID3DBlob* error_blob;
+			return D3DCompileFromFile(file_name, nullptr, nullptr, entrypoint, "cs_5_0", 0, 0, &shader_blob, &error_blob);
+		}
+
+		ID3DBlob* get_error_blob() {
+			return error_blob;
+		}
+
+		ID3DBlob* get_shader_blob() {
+			return shader_blob;
+		}
+
+		HRESULT create_UAV(ID3D11Resource* buf, ID3D11UnorderedAccessView** UAV, unsigned int size, DXGI_FORMAT format) {
+			D3D11_UNORDERED_ACCESS_VIEW_DESC UAV_desc = { };
+			UAV_desc.Format = format;
+			UAV_desc.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
+			UAV_desc.Buffer.FirstElement = 0;
+			UAV_desc.Buffer.NumElements = size;
+
+			return device->CreateUnorderedAccessView(buf, &UAV_desc, UAV);
+		}
+
+		HRESULT create_SRV(ID3D11Resource* buf, ID3D11ShaderResourceView** SRV, unsigned int size, D3D11_SRV_DIMENSION dimension) {
+			D3D11_SHADER_RESOURCE_VIEW_DESC SRV_desc = { };
+			SRV_desc.Format = DXGI_FORMAT_UNKNOWN;
+			SRV_desc.ViewDimension = dimension;
+			SRV_desc.Buffer.NumElements = size;
+			SRV_desc.Buffer.FirstElement = 0;
+
+			return device->CreateShaderResourceView(buf, &SRV_desc, SRV);
+		}
+
+		template<typename T> T* retrieve_D3D11_process(ID3D11Resource* CPU_buf, ID3D11Resource* GPU_buf) {
+			D3D11_MAPPED_SUBRESOURCE map_r = { };
+			ctx->CopyResource(CPU_buf, GPU_buf);
+
+			HRESULT hr = ctx->Map(CPU_buf, 0, D3D11_MAP_READ, 0, &map_r);
+		
+			if (FAILED(hr)) {
+				std::cout << "Mapping failed. HRESULT: " << std::hex << hr << "\n";
+				return 0;
+			}
+
+			T* dat = (T*)map_r.pData;
+			ctx->Unmap(CPU_buf, 0);
+
+			return dat;
+		}
 };
 
