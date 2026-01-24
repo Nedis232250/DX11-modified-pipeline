93fce5f4ce99742b98177866e31b84bcbd108019
Nedis232250 <97570089+Nedis232250@users.noreply.github.com>
2026-01-24 11:20:07 -0500

    Change buffers from RWBuffer to RWTexture2D

--- z.hlsl
+++ z.hlsl
@@ -1,12 +1,21 @@
-RWBuffer<uint> screen_buf : register(u0);
-RWBuffer<min16float> z_buf : register(u1);
+RWTexture2D<uint> screen_buf : register(u0);
+RWTexture2D<uint> z_buf : register(u1);
+StructuredBuffer<uint> misc : register(t0);
+groupshared half dimensions[2];
 
 [numthreads(1024, 1, 1)]
 void main(uint3 threadID : SV_DispatchThreadID) {
     uint i = threadID.x;
     
-    if (i < 1920 * 1080) {
-        screen_buf[i] = 0;
-        z_buf[i] = 1e30;
+    if (i % 1024 == 0) {
+        dimensions[0] = misc[0];
+        dimensions[1] = misc[1];
+    }
+    
+    GroupMemoryBarrierWithGroupSync();
+    
+    if (i < (dimensions[0] * dimensions[1])) {
+        screen_buf[uint2(i % dimensions[0], uint(i * rcp(float(dimensions[0]))))] = 0;
+        z_buf[uint2(i % dimensions[0], uint(i * rcp(float(dimensions[0]))))] = 4000000000;
     }
 }
