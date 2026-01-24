094794555e557def8da6b28efd74130b57b80df1
Nedis232250 <97570089+Nedis232250@users.noreply.github.com>
2026-01-24 11:19:25 -0500

    Refactor utils.hpp to add new structures and functions

--- utils.hpp
+++ utils.hpp
@@ -6,28 +6,36 @@
 #include <vector>
 #include <fstream>
 #include <chrono>
+#include <cstdint>
 #pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3dcompiler.lib")
 #define PI 3.14159
 
-float radians(float degrees) {
-	return PI * (degrees / 180);
-}
+struct Triangle {
+	float data[23];
+};
 
-template<typename T, unsigned int A, unsigned int B, unsigned int D> std::array<T, A* D> matrix_mul(const std::array<T, A* B>& x, const std::array<T, B* D>& y) {
-	std::array<T, A* D> result = {};
+struct mip_resources {
+	std::vector<ID3D11Texture2D*> mip_buffers;
+	std::vector<ID3D11Texture2D*> readback_buffers;
+	std::vector<ID3D11UnorderedAccessView*> mip_UAVs;
 
-	for (unsigned int i = 0; i < A; ++i) {
-		for (unsigned int j = 0; j < D; ++j) {
-			T sum = 0;
-			for (unsigned int k = 0; k < B; ++k) {
-				sum += x[i * B + k] * y[k * D + j];
-			}
-			result[i * D + j] = sum;
-		}
+	ID3D11Buffer* level_buf;
+	ID3D11ShaderResourceView* level_SRV;
+};
+
+float mean(std::vector<float> items) {
+	float res = 0;
+	for (unsigned int i = 0; i < items.size(); i++) {
+		res += items[i];
 	}
 
-	return result;
+	return res / items.size();
+}
+
+float radians(float degrees) {
+	return PI * (degrees / 180);
 }
 
 template<typename T> std::vector<T> add_em(T* x, int size, int more) { // Add empty memory!
@@ -50,11 +58,6 @@ void attach_console() {
 		freopen_s(&file, "CONOUT$", "w", stdout);
 	}
 }
-
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <cstdint>
 
 std::vector<unsigned int> read_bmp(std::string file_name) {
 	std::ifstream file(file_name, std::ios::in | std::ios::binary);
@@ -112,31 +115,55 @@ std::vector<unsigned int> read_bmp(std::string file_name) {
 	return img;
 }
 
+void final_buf(unsigned int* output, unsigned int* frame_buf1, unsigned int* frame_buf2, unsigned int* depth_buf1, unsigned int* depth_buf2, unsigned int size) {
+	for (unsigned int i = 0; i < size; i++) {
+		if (i < size / 2) {
+			output[i] = frame_buf1[i];
+		} else {
+			output[i] = frame_buf2[i];
+		}
+	}
+}
+
+std::string read_file(const char* filename) {
+	std::ifstream file(filename);
+
+	if (!file.is_open()) {
+		std::cerr << "Error opening file: " << filename << std::endl;
+		return 0;
+	}
+
+	std::string contents = "";
+	std::string line = "";
+
+	while (std::getline(file, line)) {
+		contents += line;
+		contents += "\n";
+	}
+
+	return contents;
+}
+
 class Shader {
 private:
 	HRESULT hr;
 	ID3D11Device* device;
-	D3D_FEATURE_LEVEL level;
 	ID3D11DeviceContext* ctx;
 	ID3DBlob* error_blob;
 	ID3DBlob* shader_blob;
 public:
 	Shader(void) {};
-	~Shader() {
-		if (shader_blob) shader_blob->Release();
-	}
 
-	void Shader_device(ID3D11Device* dev, ID3D11DeviceContext* ctx_, D3D_FEATURE_LEVEL features) {
+	void Shader_device(ID3D11Device* dev, ID3D11DeviceContext* ctx_) {
 		device = dev;
 		ctx = ctx_;
-		level = features;
 	}
 
-	HRESULT create_D3D11_buffer(ID3D11Buffer** buf, const void* dat_in, unsigned int size, unsigned char item_size, unsigned int type, unsigned int flags, unsigned int CPU_flags, D3D11_USAGE usage) {
+	HRESULT create_D3D11_buffer(ID3D11Buffer** buf, const void* dat_in, unsigned int size, unsigned char item_size, unsigned int bind, unsigned int flags, unsigned int CPU_flags, D3D11_USAGE usage) {
 		D3D11_BUFFER_DESC buffer_description = { };
 		buffer_description.ByteWidth = size;
 		buffer_description.StructureByteStride = item_size;
-		buffer_description.BindFlags = type;
+		buffer_description.BindFlags = bind;
 		buffer_description.CPUAccessFlags = CPU_flags;
 		buffer_description.MiscFlags = flags;
 		buffer_description.Usage = usage;
@@ -149,9 +176,9 @@ public:
 		return device->CreateBuffer(&buffer_description, dat_in ? &data_in : nullptr, buf);
 	}
 
-	HRESULT compile_shader(LPCWSTR file_name, LPCSTR entrypoint) {
+	HRESULT compile_shader(LPCWSTR file_name, LPCSTR entrypoint, LPCSTR model) {
 		ID3DBlob* error_blob;
-		return D3DCompileFromFile(file_name, nullptr, nullptr, entrypoint, "cs_5_0", 0, 0, &shader_blob, &error_blob);
+		return D3DCompileFromFile(file_name, nullptr, nullptr, entrypoint, model, D3DCOMPILE_OPTIMIZATION_LEVEL3, 0, &shader_blob, &error_blob);
 	}
 
 	ID3DBlob* get_error_blob() {
@@ -162,19 +189,20 @@ public:
 		return shader_blob;
 	}
 
-	HRESULT create_UAV(ID3D11Resource* buf, ID3D11UnorderedAccessView** UAV, unsigned int size, DXGI_FORMAT format) {
+	HRESULT create_UAV(ID3D11Resource* buf, ID3D11UnorderedAccessView** UAV, unsigned int size, DXGI_FORMAT format, unsigned int flags, D3D11_UAV_DIMENSION dimension) {
 		D3D11_UNORDERED_ACCESS_VIEW_DESC UAV_desc = { };
 		UAV_desc.Format = format;
-		UAV_desc.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
+		UAV_desc.ViewDimension = dimension;
 		UAV_desc.Buffer.FirstElement = 0;
 		UAV_desc.Buffer.NumElements = size;
+		UAV_desc.Buffer.Flags = flags;
 
 		return device->CreateUnorderedAccessView(buf, &UAV_desc, UAV);
 	}
 
-	HRESULT create_SRV(ID3D11Resource* buf, ID3D11ShaderResourceView** SRV, unsigned int size, D3D11_SRV_DIMENSION dimension) {
+	HRESULT create_SRV(ID3D11Resource* buf, ID3D11ShaderResourceView** SRV, unsigned int size, DXGI_FORMAT format, D3D11_SRV_DIMENSION dimension) {
 		D3D11_SHADER_RESOURCE_VIEW_DESC SRV_desc = { };
-		SRV_desc.Format = DXGI_FORMAT_UNKNOWN;
+		SRV_desc.Format = format;
 		SRV_desc.ViewDimension = dimension;
 		SRV_desc.Buffer.NumElements = size;
 		SRV_desc.Buffer.FirstElement = 0;
@@ -182,20 +210,203 @@ public:
 		return device->CreateShaderResourceView(buf, &SRV_desc, SRV);
 	}
 
-	template<typename T> T* retrieve_D3D11_process(ID3D11Resource* CPU_buf, ID3D11Resource* GPU_buf) {
+	template<typename T> void retrieve_D3D11_process(T* dest, ID3D11Resource* CPU_buf, ID3D11Resource* GPU_buf, ID3D11DeviceContext* ctx_temp, bool region, unsigned int mip_dst, unsigned int mip_src, unsigned int size) {
 		D3D11_MAPPED_SUBRESOURCE map_r = { };
-		ctx->CopyResource(CPU_buf, GPU_buf);
+		
+		if (region) {
+			ctx->CopySubresourceRegion(
+				CPU_buf,
+				mip_dst, // dst mip
+				0, 0, 0,
+				GPU_buf,
+				mip_src, // src mip
+				nullptr
+			);
+		} else {
+			ctx_temp->CopyResource(CPU_buf, GPU_buf);
+		}
 
-		HRESULT hr = ctx->Map(CPU_buf, 0, D3D11_MAP_READ, 0, &map_r);
+   		HRESULT hr = ctx_temp->Map(CPU_buf, 0, D3D11_MAP_READ, 0, &map_r);
 
 		if (FAILED(hr)) {
 			std::cout << "Mapping failed. HRESULT: " << std::hex << hr << "\n";
-			return 0;
 		}
 
-		T* dat = (T*)map_r.pData;
-		ctx->Unmap(CPU_buf, 0);
-
-		return dat;
+		std::memcpy(dest, map_r.pData, size);
+		ctx_temp->Unmap(CPU_buf, 0);
 	}
 };
+
+void CPU_vertex_transformation(std::vector<float>& sogp, std::vector<float>& spositions, unsigned int triangle_count_1, float thetax, float thetay) {
+	for (unsigned int i = 0; i < triangle_count_1 * 9; i += 3) {
+		float x = sogp[i];
+		float y = sogp[i + 1];
+		float z = 0.1f * (sogp[i + 2] + 10);
+
+		float ry = radians(thetax);
+		float rx = radians(thetay);
+
+		float y1 = y * cos(rx) - z * sin(rx);
+		float z1 = y * sin(rx) + z * cos(rx);
+		float x1 = x;
+
+		float xr = x1 * cos(ry) + z1 * sin(ry);
+		float yr = y1;
+		float zr = -x1 * sin(ry) + z1 * cos(ry);
+
+		float invz = 1.0f / zr;
+		float xp = (xr * invz * 0.5f + 0.5f);
+		float yp = (yr * invz * -0.5f + 0.5f);
+
+		spositions[i] = xp;
+		spositions[i + 1] = yp;
+		spositions[i + 2] = zr;
+	}
+
+	for (unsigned int i = 0; i < triangle_count_1; i++) {
+		if (spositions[i * 9 + 2] <= 0.25 || spositions[i * 9 + 5] <= 0.25 || spositions[i * 9 + 8] <= 0.25) {
+			spositions[i * 9] = 0;
+			spositions[i * 9 + 1] = 0;
+			spositions[i * 9 + 2] = 0;
+			spositions[i * 9 + 3] = 0;
+			spositions[i * 9 + 4] = 0;
+			spositions[i * 9 + 5] = 0;
+			spositions[i * 9 + 6] = 0;
+			spositions[i * 9 + 7] = 0;
+			spositions[i * 9 + 8] = 0;
+		}
+	}
+}
+
+void CPU_sort_shader(std::vector<float>& spositions, std::vector<float>& scolors, std::vector<Triangle>& triangles, unsigned int triangle_count_1) {
+	for (unsigned int i = 0; i < triangle_count_1; i++) {
+		triangles[i] = {
+			{
+				spositions[i * 9],
+				spositions[i * 9 + 1],
+				spositions[i * 9 + 2],
+				spositions[i * 9 + 3],
+				spositions[i * 9 + 4],
+				spositions[i * 9 + 5],
+				spositions[i * 9 + 6],
+				spositions[i * 9 + 7],
+				spositions[i * 9 + 8],
+				scolors[i * 13],
+				scolors[i * 13 + 1],
+				scolors[i * 13 + 2],
+				scolors[i * 13 + 3],
+				scolors[i * 13 + 4],
+				scolors[i * 13 + 5],
+				scolors[i * 13 + 6],
+				scolors[i * 13 + 7],
+				scolors[i * 13 + 8],
+				scolors[i * 13 + 9],
+				scolors[i * 13 + 10],
+				scolors[i * 13 + 11],
+				scolors[i * 13 + 12],
+				((spositions[i * 9 + 8] * 10 - 10) + (spositions[i * 9 + 5] * 10 - 10) + (spositions[i * 9 + 2] * 10 - 10)) / 3.0f
+			}
+		};
+	}
+
+	std::sort(triangles.begin(), triangles.end(),
+		[](const Triangle& a, const Triangle& b) {
+			return a.data[22] < b.data[22];
+		});
+
+	for (unsigned int i = 0; i < triangle_count_1; i++) {
+		spositions[i * 9] = triangles[i].data[0];
+		spositions[i * 9 + 1] = triangles[i].data[1];
+		spositions[i * 9 + 2] = triangles[i].data[2];
+		spositions[i * 9 + 3] = triangles[i].data[3];
+		spositions[i * 9 + 4] = triangles[i].data[4];
+		spositions[i * 9 + 5] = triangles[i].data[5];
+		spositions[i * 9 + 6] = triangles[i].data[6];
+		spositions[i * 9 + 7] = triangles[i].data[7];
+		spositions[i * 9 + 8] = triangles[i].data[8];
+		scolors[i * 13] = triangles[i].data[9];
+		scolors[i * 13 + 1] = triangles[i].data[10];
+		scolors[i * 13 + 2] = triangles[i].data[11];
+		scolors[i * 13 + 3] = triangles[i].data[12];
+		scolors[i * 13 + 4] = triangles[i].data[13];
+		scolors[i * 13 + 5] = triangles[i].data[14];
+		scolors[i * 13 + 6] = triangles[i].data[15];
+		scolors[i * 13 + 7] = triangles[i].data[16];
+		scolors[i * 13 + 8] = triangles[i].data[17];
+		scolors[i * 13 + 9] = triangles[i].data[18];
+		scolors[i * 13 + 10] = triangles[i].data[19];
+		scolors[i * 13 + 11] = triangles[i].data[20];
+		scolors[i * 13 + 12] = triangles[i].data[21];
+	}
+}
+
+void CPU_triangle_metadata(std::vector<float>& tri_meta, std::vector<float>& spositions, unsigned int triangle_count_1, unsigned short width, unsigned short height) {
+	for (unsigned int i = 0; i < triangle_count_1; i++) {
+		spositions[i * 9] *= width;
+		spositions[i * 9 + 1] *= height;
+		spositions[i * 9 + 3] *= width;
+		spositions[i * 9 + 4] *= height;
+		spositions[i * 9 + 6] *= width;
+		spositions[i * 9 + 7] *= height;
+
+		int minx = std::min(spositions[i * 9], std::min(spositions[i * 9 + 3], spositions[i * 9 + 6]));
+		int miny = std::min(spositions[i * 9 + 1], std::min(spositions[i * 9 + 4], spositions[i * 9 + 7]));
+		int maxx = std::max(spositions[i * 9], std::max(spositions[i * 9 + 3], spositions[i * 9 + 6]));
+		int maxy = std::max(spositions[i * 9 + 1], std::max(spositions[i * 9 + 4], spositions[i * 9 + 7]));
+
+		float inv_area = 1.0f / ((spositions[i * 9] - spositions[i * 9 + 3]) * (spositions[i * 9 + 7] - spositions[i * 9 + 4]) - (spositions[i * 9 + 1] - spositions[i * 9 + 4]) * (spositions[i * 9 + 6] - spositions[i * 9 + 3]));
+		float v3ymv2y = spositions[i * 9 + 7] - spositions[i * 9 + 4];
+		float v1ymv3y = spositions[i * 9 + 1] - spositions[i * 9 + 7];
+		float v3xmv2x = spositions[i * 9 + 6] - spositions[i * 9 + 3];
+		float v1xmv3x = spositions[i * 9] - spositions[i * 9 + 6];
+
+		tri_meta.push_back((float)minx);
+		tri_meta.push_back((float)miny);
+		tri_meta.push_back((float)maxx);
+		tri_meta.push_back((float)maxy);
+		tri_meta.push_back(inv_area);
+		tri_meta.push_back(v3ymv2y);
+		tri_meta.push_back(v1ymv3y);
+		tri_meta.push_back(v3xmv2x);
+		tri_meta.push_back(v1xmv3x);
+		tri_meta.push_back(v3ymv2y * inv_area);
+		tri_meta.push_back(-v3xmv2x * inv_area);
+		tri_meta.push_back(v1ymv3y * inv_area);
+		tri_meta.push_back(-v1xmv3x * inv_area);
+	}
+}
+
+struct triangle_everything {
+	std::vector<float> positions;
+	std::vector<float> colors;
+	std::vector<float> tri_meta;
+	unsigned int triangles_passed;
+};
+
+triangle_everything passed_filter(std::vector<float> spositions, std::vector<float> scolors, std::vector<float> tri_meta, std::vector<unsigned int> occluder_results) {
+	triangle_everything result = {};
+	std::vector<float> positions;
+	std::vector<float> colors;
+	std::vector<float> meta;
+	unsigned int passed = 0;
+
+	for (unsigned int i = 0; i < occluder_results.size(); i++) {
+		if (occluder_results[i] == 1) {
+			for (unsigned int j = 0; j < 9; j++) {
+				positions.push_back(spositions[i * 9 + j]);
+			}
+			for (unsigned int j = 0; j < 13; j++) {
+				colors.push_back(scolors[i * 13 + j]);
+				meta.push_back(tri_meta[i * 13 + j]);
+			}
+			passed++;
+		}
+	}
+
+	result.colors = colors;
+	result.positions = positions;
+	result.triangles_passed = passed;
+	result.tri_meta = meta;
+
+	return result;
+}
